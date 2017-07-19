a8a309f82f76f6cb9257da0096fb0d25ec6aaa77
Michal Karwanski <karwer@gmail.com>
2017-07-19 13:08:29 +0200

    Add dependency to errorless chatterbox

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule GRPC.Mixfile do
      # TODO
      # https://github.com/joedevivo/chatterbox/issues/57
      # https://github.com/joedevivo/chatterbox/issues/93
-     {:chatterbox, github: "tony612/chatterbox", branch: "my-fix"},
+     {:chatterbox, github: "karwer/chatterbox", branch: "my-fix"},
      # TODO
      # https://github.com/ninenines/cowboy/pull/1020
 		 # https://github.com/ninenines/cowboy/pull/1068
